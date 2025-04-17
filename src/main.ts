86c36cc8155b630b003a0b19f177d3556c3ccdc3
Bryan Acosta <bxacosta@hotmail.com>
2025-04-17 04:03:14 -0500

    refactor: optimize HTML, CSS and use CSS variables for colors

--- src/main.ts
+++ src/main.ts
@@ -4,33 +4,31 @@ import "./global.css";
 function noSearchDefaultPageRender() {
     const app = document.querySelector<HTMLDivElement>("#app")!;
     app.innerHTML = `
-    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
-      <div class="content-container">
-        <h1>Und*ck</h1>
-        <p>DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a></p>
-        <div class="url-container"> 
-          <input 
-            type="text" 
-            class="url-input"
-            value="https://unduck.link?q=%s"
-            readonly 
-          />
-          <button class="copy-button">
-            <img src="/clipboard.svg" alt="Copy" />
-          </button>
-        </div>
-      </div>
-      <footer class="footer">
-        <a href="https://t3.chat" target="_blank">t3.chat</a>
-        •
-        <a href="https://x.com/theo" target="_blank">theo</a>
-        •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
-      </footer>
-    </div>
-  `;
+      <main class="main-container">
+        <section class="content-container">
+          <h1>Und*ck</h1>
+          <p>
+            DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables
+            <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a>
+          </p>
+          <div class="url-container">
+            <input type="text" class="url-input" value="https://unduck.link?q=%s" readonly/>
+            <button id="copy-button" class="icon-button" aria-label="Copy URL">
+              <img src="/clipboard.svg" alt="Copy" />
+            </button>
+          </div>
+        </section>
+        <footer class="footer">
+          <a href="https://t3.chat" target="_blank">t3.chat</a>
+          •
+          <a href="https://x.com/theo" target="_blank">theo</a>
+          •
+          <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
+        </footer>
+      </main>
+    `;
 
-    const copyButton = app.querySelector<HTMLButtonElement>(".copy-button")!;
+    const copyButton = app.querySelector<HTMLButtonElement>("#copy-button")!;
     const copyIcon = copyButton.querySelector("img")!;
     const urlInput = app.querySelector<HTMLInputElement>(".url-input")!;
 
