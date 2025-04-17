abb206221d58ba4f990498cbc4a2e0930c2b2a74
Bryan Acosta <bxacosta@hotmail.com>
2025-04-17 02:34:53 -0500

    refactor: add type and clean unused bangs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { bangs } from "./bang";
+import {bangs} from "./bang";
 import "./global.css";
 
 function noSearchDefaultPageRender() {
-  const app = document.querySelector<HTMLDivElement>("#app")!;
-  app.innerHTML = `
+    const app = document.querySelector<HTMLDivElement>("#app")!;
+    app.innerHTML = `
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
       <div class="content-container">
         <h1>Und*ck</h1>
@@ -30,55 +30,56 @@ function noSearchDefaultPageRender() {
     </div>
   `;
 
-  const copyButton = app.querySelector<HTMLButtonElement>(".copy-button")!;
-  const copyIcon = copyButton.querySelector("img")!;
-  const urlInput = app.querySelector<HTMLInputElement>(".url-input")!;
+    const copyButton = app.querySelector<HTMLButtonElement>(".copy-button")!;
+    const copyIcon = copyButton.querySelector("img")!;
+    const urlInput = app.querySelector<HTMLInputElement>(".url-input")!;
 
-  copyButton.addEventListener("click", async () => {
-    await navigator.clipboard.writeText(urlInput.value);
-    copyIcon.src = "/clipboard-check.svg";
+    copyButton.addEventListener("click", async () => {
+        await navigator.clipboard.writeText(urlInput.value);
+        copyIcon.src = "/clipboard-check.svg";
 
-    setTimeout(() => {
-      copyIcon.src = "/clipboard.svg";
-    }, 2000);
-  });
+        setTimeout(() => {
+            copyIcon.src = "/clipboard.svg";
+        }, 2000);
+    });
 }
 
 const LS_DEFAULT_BANG = localStorage.getItem("default-bang") ?? "g";
-const defaultBang = bangs.find((b) => b.t === LS_DEFAULT_BANG);
+const defaultBang = bangs.find((bang) => bang.trigger === LS_DEFAULT_BANG);
 
-function getBangredirectUrl() {
-  const url = new URL(window.location.href);
-  const query = url.searchParams.get("q")?.trim() ?? "";
-  if (!query) {
-    noSearchDefaultPageRender();
-    return null;
-  }
+function getBangRedirectUrl() {
+    const url = new URL(window.location.href);
+    const query = url.searchParams.get("q")?.trim() ?? "";
 
-  const match = query.match(/!(\S+)/i);
+    if (!query) {
+        noSearchDefaultPageRender();
+        return null;
+    }
 
-  const bangCandidate = match?.[1]?.toLowerCase();
-  const selectedBang = bangs.find((b) => b.t === bangCandidate) ?? defaultBang;
+    const match = query.match(/!(\S+)/i);
 
-  // Remove the first bang from the query
-  const cleanQuery = query.replace(/!\S+\s*/i, "").trim();
+    const bangCandidate = match?.[1]?.toLowerCase();
+    const selectedBang = bangs.find((bang) => bang.trigger === bangCandidate) ?? defaultBang;
 
-  // Format of the url is:
-  // https://www.google.com/search?q={{{s}}}
-  const searchUrl = selectedBang?.u.replace(
-    "{{{s}}}",
-    // Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
-    encodeURIComponent(cleanQuery).replace(/%2F/g, "/")
-  );
-  if (!searchUrl) return null;
+    // Remove the first bang from the query
+    const cleanQuery = query.replace(/!\S+\s*/i, "").trim();
 
-  return searchUrl;
+    // Format of the url is:
+    // https://www.google.com/search?q={{{s}}}
+    const searchUrl = selectedBang?.url.replace(
+        "{{{s}}}",
+        // Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
+        encodeURIComponent(cleanQuery).replace(/%2F/g, "/")
+    );
+    if (!searchUrl) return null;
+
+    return searchUrl;
 }
 
 function doRedirect() {
-  const searchUrl = getBangredirectUrl();
-  if (!searchUrl) return;
-  window.location.replace(searchUrl);
+    const searchUrl = getBangRedirectUrl();
+    if (!searchUrl) return;
+    window.location.replace(searchUrl);
 }
 
 doRedirect();
